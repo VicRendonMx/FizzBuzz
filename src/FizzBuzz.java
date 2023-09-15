4b9d109f977c30c17b3541171002ecb6e5bde63f
VicRendonMx <144366264+VicRendonMx@users.noreply.github.com>
2023-09-15 12:20:00 -0400

    Update FizzBuzz.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,7 +6,7 @@ class FizzBuzz {
     public static void main(String[] args) {
 
         int i = 0;
-        int a = 32;
+        int a = 31;
         while (i < 100) {
 
             i = doFizzBuzz(i);
